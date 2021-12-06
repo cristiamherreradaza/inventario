11b27fdd6dc592a7d263de3e161cdf2def3bc9c5
Cristiam Herrera <cristiamherrera@gmail.com>
2021-12-05 20:28:27 -0400

    Se cambio la importacion del excel

--- app/Imports/ProductosImport.php
+++ app/Imports/ProductosImport.php
@@ -28,225 +28,43 @@ class ProductosImport implements ToModel, WithStartRow
     */
     public function model(array $row)
     {
-        $configuraciones = Configuracione::where('descripcion', 'generacionCodigos')->first();
-
-        if ($configuraciones->valor == 'Si') {
-            // quitamos los espacios de la marca
-            $marcaExcel = trim($row[5]);
-            $busca_marca = Marca::where('nombre', 'like', "%$marcaExcel%")
-                ->first();
-
-            if ($busca_marca == null) {
-                $marca = new Marca();
-                $marca->user_id = Auth::user()->id;
-                $marca->nombre = $marcaExcel;
-                $marca->save();
-                $marca_id = $marca->id;
-                $sigla_marca = $this->extraeCodigo($marcaExcel);
-            } else {
-                $marca_id = $busca_marca->id;
-                $sigla_marca = $this->extraeCodigo($busca_marca->nombre);
-            }
-
-            $tipoExcel = trim($row[4]);
-            $busca_tipo = Tipo::where('nombre', 'like', "%$tipoExcel%")
-                ->first();
-
-            if ($busca_tipo == null) {
-                $tipo = new Tipo();
-                $tipo->user_id = Auth::user()->id;
-                $tipo->nombre = $tipoExcel;
-                $tipo->save();
-                $tipo_id = $tipo->id;
-                $sigla_tipo = $this->extraeCodigo($tipoExcel);
-            } else {
-                $tipo_id = $busca_tipo->id;
-                $sigla_tipo = $this->extraeCodigo($busca_tipo->nombre);
-            }
-
-            $categoriaExcel = trim($row[3]);
-            $busca_categoria = Categoria::where('nombre', 'like', "%$categoriaExcel%")
-                ->first();
-
-            if ($busca_categoria == null) {
-                $categoria = new Categoria();
-                $categoria->user_id = Auth::user()->id;
-                $categoria->nombre = $categoriaExcel;
-                $categoria->save();
-                $categoria_id = $categoria->id;
-                $sigla_categoria = $this->extraeCodigo($categoriaExcel);
-            } else {
-                $categoria_id = $busca_categoria->id;
-                $sigla_categoria = $this->extraeCodigo($busca_categoria->nombre);
-            }
-            $nombreExcel = trim($row[1]);
-            $sigla_nombre = $this->extraeCodigo($nombreExcel);
-
-            $codigoGenerado = $sigla_marca . '-' . $sigla_tipo . '-' . $sigla_nombre;
-
-        } else {
-
-            $marcaExcel = trim($row[5]);
-            $busca_marca = Marca::where('nombre', 'like', "%$marcaExcel%")
-                ->first();
-
-            if ($busca_marca == null) {
-                $marca = new Marca();
-                $marca->user_id = Auth::user()->id;
-                $marca->nombre = $marcaExcel;
-                $marca->save();
-                $marca_id = $marca->id;
-            } else {
-                $marca_id = $busca_marca->id;
-            }
-
-            $tipoExcel = trim($row[4]);
-            $busca_tipo = Tipo::where('nombre', 'like', "%$tipoExcel%")
-                ->first();
-
-            if ($busca_tipo == null) {
-                $tipo = new Tipo();
-                $tipo->user_id = Auth::user()->id;
-                $tipo->nombre = $tipoExcel;
-                $tipo->save();
-                $tipo_id = $tipo->id;
-            } else {
-                $tipo_id = $busca_tipo->id;
-            }
-
-            $categoriaExcel = trim($row[3]);
-            $busca_categoria = Categoria::where('nombre', 'like', "%$categoriaExcel%")
-                ->first();
-
-            if ($busca_categoria == null) {
-                $categoria = new Categoria();
-                $categoria->user_id = Auth::user()->id;
-                $categoria->nombre = $categoriaExcel;
-                $categoria->save();
-                $categoria_id = $categoria->id;
-            } else {
-                $categoria_id = $busca_categoria->id;
-            }
-
-            $nombreExcel = trim($row[1]);
-            $codigoGenerado = $row[26];
-        }
-
+       
+        //echo "Nombre ".$row[]
         $producto                  = new Producto();
         $producto->user_id         = Auth::user()->id;
-        $producto->marca_id        = $marca_id;
-        $producto->tipo_id         = $tipo_id;
-        $producto->codigo          = $codigoGenerado;
-        $producto->nombre          = $nombreExcel;
-        $producto->nombre_venta    = ($row[2] == "") ? null : $row[2];
-        $producto->modelo          = ($row[6] == "") ? null : $row[6];
-        $producto->precio_compra   = ($row[7] == "") ? 0 : $row[7];
-        $producto->cantidad_minima = ($row[10] == "") ? 1 : $row[10];
-        $producto->largo           = ($row[13] == "") ? 0 : $row[13];
-        $producto->ancho           = ($row[14] == "") ? 0 : $row[14];
-        $producto->alto            = ($row[15] == "") ? 0 : $row[15];
-        $producto->peso            = ($row[16] == "") ? 0 : $row[16];
-        $producto->colores         = ($row[17] == "") ? null : $row[17];
-        $producto->descripcion     = ($row[23] == "") ? null : $row[23];
-        $producto->url_referencia  = ($row[24] == "") ? null : $row[24];
-        $producto->video           = ($row[25] == "") ? null : $row[25];
+        $producto->marca_id        = 1;
+        $producto->tipo_id         = 1;
+        $producto->codigo          = $row[4];
+        $producto->nombre          = $row[2];
+        $producto->nombre_venta    = $row[3];
         $producto->save();
         $producto_id = $producto->id;
 
-        if($configuraciones->valor == 'Si')
-        {
-            // se le asigna un numero al producto creado
-            $cambia_codigo = Producto::find($producto_id);
-            $numeroProducto = str_pad($producto_id, 5, "0", STR_PAD_LEFT);
-            $cambia_codigo->codigo = $codigoGenerado.'-'.$numeroProducto;
-            $cambia_codigo->save();
-        }
-
-        $busca_proveedor = Proveedore::where('nombre', 'like', "%$row[12]%")
+        $busca_proveedor = Proveedore::where('nombre', 'like', "%$row[1]%")
             ->first();
         if ($busca_proveedor == null) {
             $proveedor = new Proveedore();
             $proveedor->user_id = Auth::user()->id;
-            $proveedor->nombre = $row[12];
+            $proveedor->nombre = $row[1];
             $proveedor->save();
             $proveedorId = $proveedor->id;
         } else {
             $proveedorId = $busca_proveedor->id;
         }
 
-        $busca_almacen = Almacene::where('nombre', 'like', "%$row[11]%")
-            ->first();
-        if ($busca_almacen == null) {
-            $almacene_id = 1;
-        } else {
-            $almacene_id = $busca_almacen->id;
-        }
-
+       
         $movimiento                = new Movimiento();
         $movimiento->user_id       = Auth::user()->id;
         $movimiento->producto_id   = $producto_id;
-        $movimiento->almacene_id   = $almacene_id;
-        $movimiento->proveedor_id  = $proveedorId;
-        $movimiento->tipo_id       = $tipo_id;
+        $movimiento->almacene_id   = 1;
+        $movimiento->proveedor_id  = 1;
+        $movimiento->tipo_id       = 1;
         $movimiento->fecha         = date("Y-m-d H:i:s");
-        $movimiento->precio_compra = $row[7];
-        $movimiento->precio_venta  = 0;
-        $movimiento->ingreso       = $row[9];
+        $movimiento->ingreso       = $row[8];
         $movimiento->estado        = "Ingreso";
         $movimiento->save();
 
-        $categoriasProducto = new CategoriasProducto();
-        $categoriasProducto->user_id = Auth::user()->id;
-        $categoriasProducto->categoria_id = $categoria_id;
-        $categoriasProducto->producto_id = $producto_id;
-        $categoriasProducto->save();
-
-        $precio = new Precio();
-        $precio->user_id = Auth::user()->id;
-        $precio->producto_id = $producto_id;
-        $precio->escala_id = 1;
-        $precio->precio = $row[8];
-        $precio->save();
-
-        if ($row[18] != '') {
-            $caracteristicas = new Caracteristica();
-            $caracteristicas->user_id = Auth::user()->id;
-            $caracteristicas->producto_id = $producto_id;
-            $caracteristicas->descripcion = $row[18];
-            $caracteristicas->save();
-        }
-
-        if ($row[19] != '') {
-            $caracteristicas = new Caracteristica();
-            $caracteristicas->user_id = Auth::user()->id;
-            $caracteristicas->producto_id = $producto_id;
-            $caracteristicas->descripcion = $row[19];
-            $caracteristicas->save();
-        }
-        
-        if ($row[20] != '') {
-            $caracteristicas = new Caracteristica();
-            $caracteristicas->user_id = Auth::user()->id;
-            $caracteristicas->producto_id = $producto_id;
-            $caracteristicas->descripcion = $row[20];
-            $caracteristicas->save();
-        }
-
-        if ($row[21] != '') {
-            $caracteristicas = new Caracteristica();
-            $caracteristicas->user_id = Auth::user()->id;
-            $caracteristicas->producto_id = $producto_id;
-            $caracteristicas->descripcion = $row[21];
-            $caracteristicas->save();
-        }
-
-        if ($row[22] != '') {
-            $caracteristicas = new Caracteristica();
-            $caracteristicas->user_id = Auth::user()->id;
-            $caracteristicas->producto_id = $producto_id;
-            $caracteristicas->descripcion = $row[22];
-            $caracteristicas->save();
-        }
+       
     }
 
     function extraeCodigo($texto)
